4d8d58a4253d15bf2ad013264459367698912d57
Heba Youssef <hebamyoussef7@gmail.com>
2023-12-14 18:14:26 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that program that prints aline followed by a new line to the standard error
+ * main - A program that prints aline followed by a new line to the standard error
  * Return: 1 (Success)
  **/
 
 int main(void)
 {
-       fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+       fputs("and that piece of art is useful\" - Dora Korpar
+		       , 2015-10-19\n", stdout);
        return (1);
 }
 

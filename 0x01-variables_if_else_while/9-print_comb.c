9311a80b29c28ee35c26d8ef2481271336a3697f
Heba Youssef <hebamyoussef7@gmail.com>
2023-12-18 17:17:49 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - all possible combinations of single digits
+ * Return: Always 0 (Success)
+ *
+ **/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	putchar(a);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

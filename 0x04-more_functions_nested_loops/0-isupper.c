52498d0d234706aef568e9cb5bca101687d2790b
Heba Youssef <hebamyoussef7@gmail.com>
2023-12-22 17:24:48 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,16 @@
 /**
  * _isupper - check if a letter is upper
  * @c: The letter  to be checked
- * Return: 1 for uppercase 0 for any other 
+ * Return: 1 for uppercase 0 for any other
  */
 
 int _isupper(int c)
 {
-	
+
 	if (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
 	return (0);
 }
+

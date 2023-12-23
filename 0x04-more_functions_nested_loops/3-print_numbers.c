902ef6e65cbfe0ecf1cf91e3a5986bdda6bf539c
Heba Youssef <hebamyoussef7@gmail.com>
2023-12-22 17:51:36 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,16 @@
 /**
  * print_numbers - prints numbers from 0 to 9
  * Return: returns printed numbers
- */
+ **/
 
 void print_numbers(void)
 {
+	
 	int x;
-
+	
 	for (x = 0; x <= 9; x++)
 	{
 	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
-

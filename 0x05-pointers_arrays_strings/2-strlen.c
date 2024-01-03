5e3d7470751eb94d5876bf4456bbaae4e8c774bf
Heba Youssef <hebamyoussef7@gmail.com>
2024-01-03 15:47:15 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @str: the string thats length is needed
+ * Return: The length of @str
+ */
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*str++)
+	length++;
+	return (length);
+}
+

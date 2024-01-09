2b765af6c0f6f3b04d4f3827cd4a575419010182
Heba Youssef <hebamyoussef7@gmail.com>
2024-01-08 17:32:44 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, found, count;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	found = 0;	
+
+	for (j=0; accept[j] != '\0'; j++)
+	{
+	if (accept[j] == s[i])
+	{
+	found = 1;
+	break;
+	}
+	}
+
+	if (found == 0)
+	{
+	break;
+	}
+	count++;
+	}
+	return (count);
+}
+
